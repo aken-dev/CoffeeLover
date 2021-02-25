d25cc9048d93a16325b4f68a7bdcc9292e3aa2f3
aokiken <aokiken@localhost>
2021-02-25 17:04:31 +0900

    スペルミス修正

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
 			System.out.println("どんな珈琲を作りますか？");
 			System.out.print("(1)ホットコーヒー　(2)アイスコーヒー　(3)コーヒーゼリー　" +
 				 "(0)終了：");
-			int choise = sc.nextInt();
-			if (choise == 0) break;
-			switch(choise) {
+			int choice = sc.nextInt();
+			if (choice == 0) break;
+			switch(choice) {
 				case 1:
 					System.out.println("【ホットコーヒーを作ります。】");
 					HotCoffee hc = new HotCoffee(90, 550, 5, 30);
