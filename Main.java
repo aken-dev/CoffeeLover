59e554c274c2556871b0da5738c4179b90fc58c1
青木健 <63615354+aken-dev@users.noreply.github.com>
2021-06-03 22:56:40 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		// TODO 自動生成されたメソッド・スタブ
 		Scanner sc = new Scanner(System.in);
-		
+
 		while(true) {
 			System.out.println("どんな珈琲を作りますか？");
 			System.out.print("(1)ホットコーヒー　(2)アイスコーヒー　(3)コーヒーゼリー　" +
 				 "(0)終了：");
-			int choice = sc.nextInt();
-			if (choice == 0) break;
-			switch(choice) {
+			int choise = sc.nextInt();
+			if (choise == 0) break;
+			switch(choise) {
 				case 1:
 					System.out.println("【ホットコーヒーを作ります。】");
 					HotCoffee hc = new HotCoffee(90, 550, 5, 30);
@@ -19,7 +19,7 @@ public class Main {
 					break;
 				case 2:
 					System.out.println("【アイスコーヒーを作ります。】");
-					IcedCoffee ic = new IcedCoffee(88, 300, 5, 45, 250);
+					IcedCoffee ic = new IcedCoffee(90, 300, 5, 45, 250);
 					int liquid_ic = ic.making();
 					break;
 				case 3:	
